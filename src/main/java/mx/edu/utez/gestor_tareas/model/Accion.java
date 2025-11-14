2e9d08e3aae6a71cc00a14207bdb71bbf06feb52
Lee-Itzel <20243ds010@utez.edu.mx>
2025-11-13 20:02:44 -0600

    Finalizado la clase de pila

--- src/main/java/mx/edu/utez/gestor_tareas/model/Accion.java
+++ src/main/java/mx/edu/utez/gestor_tareas/model/Accion.java
@@ -20,4 +20,36 @@ public class Accion {
         this.descripcion = descripcion;
         this.fechaHora = LocalDateTime.now();
     }
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    @Override
+    public String toString() {
+        return "Accion{" +
+                "tipo='" + tipo + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", fechaHora=" + fechaHora +
+                '}';
+    }
 }
